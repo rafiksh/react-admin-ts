73798d7a92485a4e193602933c25fe6a49047c9c
Rafik <rafikshmoury@gmail.com>
2022-07-04 16:13:22 +0300

    remove: antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import { useEffect } from "react";
-import { ConfigProvider, Spin } from "antd";
 import { useTranslation } from "react-i18next";
-import { usePromiseTracker } from "react-promise-tracker";
 
 import "./App.css";
-import "antd/dist/antd.css";
 import { AppRouter } from "./App.router";
 
 const App = () => {
   const { i18n } = useTranslation();
-  const { promiseInProgress } = usePromiseTracker();
 
   /** This useEffect rerenders dir */
   useEffect(() => {}, [i18n.language]);
 
-  return (
-    /* This wrapper handles rtl and ltr directions for i18n */
-    <ConfigProvider direction={i18n.dir()}>
-      <AppRouter />
-      <Spin spinning={promiseInProgress} />
-    </ConfigProvider>
-  );
+  return <AppRouter />;
 };
 
 export { App };
--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ ReactDOM.render(
   <I18nextProvider i18n={i18n}>
     <App />
   </I18nextProvider>,
-  document.getElementById("root")
+  document.getElementById("root"),
 );
 
 // If you want to start measuring performance in your app, pass a function
