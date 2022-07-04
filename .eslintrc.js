07a44f8161aa246ca5a6cfdf3c4a6efc6423a698
Rafik <rafikshmoury@gmail.com>
2022-07-04 16:12:43 +0300

    chore: force linting

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  extends: "react-app",
+  plugins: ["react-hooks"],
+  rules: {
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+  },
+};
